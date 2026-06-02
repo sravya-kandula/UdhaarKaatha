b1f5650f91e79bd52f12a50ca8ccabf50860767b
kandula sravya <sravyakandula194@gmail.com>
2026-06-02 19:17:39 +0530

    Refactor CORS configuration to use array syntax

--- Backend/server.js
+++ Backend/server.js
@@ -22,11 +22,13 @@ checkOverdueCustomers();
 
 app.use(
   cors({
-    origin: "https://udhaar-kaatha.vercel.app",
-  "https://udhaar-kaatha-git-main-sravya-kandulas-projects.vercel.app",
-  "https://udhaar-kaatha-lsbsb6lc7-sravya-kandulas-projects.vercel.app",
+    origin: [
+      "https://udhaar-kaatha.vercel.app",
+      "https://udhaar-kaatha-git-main-sravya-kandulas-projects.vercel.app",
+      "https://udhaar-kaatha-lsbsb6lc7-sravya-kandulas-projects.vercel.app",
+    ],
     credentials: true,
-  }),
+  })
 );
 app.use(express.json());
 app.use("/api/payment", paymentRoutes);
